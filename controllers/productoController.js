6248d3b42e6fe3920c857a180cddc4f10bf7bbe9
Robinson Elias Maqui Canaviri <rmaqui.trebolit@bancochile.cl>
2020-08-31 18:16:48 -0400

    Hasta Palindromo

--- controllers/productoController.js
+++ controllers/productoController.js
@@ -1,4 +1,41 @@
 const { response } = require('express');
+const isPalindrome = require('../util/util');
+
+function filterInt(value) {
+    if (/^[-+]?(\d+|Infinity)$/.test(value)) {
+      return Number(value)
+    } else {
+      return NaN
+    }
+ };
+
+ const dataFind = ( palabraBuscada ) =>{
+
+    let busqueda = null;
+
+    let idNumerico = filterInt(palabraBuscada);
+
+    if(palabraBuscada.length > 3 ){        
+        if(idNumerico){
+            busqueda = [
+                {"id": palabraBuscada }, 
+                {"brand":{$regex: ".*" + palabraBuscada + ".*"}}, 
+                {"description": {$regex: ".*" + palabraBuscada + ".*"}}
+            ];
+        }else{
+            busqueda = [
+                {"brand":{$regex: ".*" + palabraBuscada + ".*"}}, 
+                {"description": {$regex: ".*" + palabraBuscada + ".*"}}
+            ];
+        }    
+    }else{
+        if(idNumerico){
+            busqueda = [{"id": palabraBuscada}];    
+        }
+    }
+
+    return busqueda;
+};
 
 const buscarProducto = async (req, res = response ) => {
 
@@ -7,9 +44,44 @@ const buscarProducto = async (req, res = response ) => {
     // Generar JWT
     //const token = await generarJWT( uid, name );
 
-    res.json({
-        ok: true
-    })
+
+    try{
+        let busqueda = dataFind(req.query.search);
+        let respuesta = {
+            codigo: "00",
+            productos: null,
+            isPalindromo: false
+        }
+        if(busqueda){
+            const busquedaNormalizada = JSON.stringify(busqueda);
+
+            let palabraSearchIsPalindromo = isPalindrome( req.query.search );            
+
+            //let productos = await Producto.find({ $or : JSON.parse(busquedaNormalizada) });
+            
+            //respuesta.codigo = productos && productos.length > 0 ? "00":"02";
+
+            if ( palabraSearchIsPalindromo ){
+                respuesta.isPalindromo = palabraSearchIsPalindromo;
+                //productos.forEach(prod => prod.price = (prod.price / 2));
+            }
+
+            //respuesta.productos = productos;
+            console.log('respuesta', respuesta);
+            res.json(respuesta);
+        }else{
+            respuesta.codigo = "01";
+            res.json(respuesta);
+        }
+
+        /*res.json({
+            ok: true
+        })*/
+    
+    }catch (error){
+        console.error(error);
+        res.status(400).json({ msg: 'Hubo un error en la busqueda del producto'});
+    }    
 }
 
 
