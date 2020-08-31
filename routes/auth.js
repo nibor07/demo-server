3fe57dda0d4050a7a6188387d694536bea1cbd92
Robinson Elias Maqui Canaviri <rmaqui.trebolit@bancochile.cl>
2020-08-31 17:51:54 -0400

    get

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,13 @@
 const { Router } = require('express');
+const { revalidarToken } = require('../controllers/auth');
 const router = Router();
 
-router.get('/', (req, res) => {
+/*router.get('/', (req, res) => {
     res.json({
         ok:true
     })
-});
+});*/
+
+router.get('/', revalidarToken );
 
 module.exports = router;
