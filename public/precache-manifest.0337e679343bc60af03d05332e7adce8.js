d0007a3e5ebf35103bc4ce8f44eefafbc4a87741
Robinson Elias Maqui Canaviri <rmaqui.trebolit@bancochile.cl>
2020-08-31 21:56:53 -0400

    react

---
+++ public/precache-manifest.0337e679343bc60af03d05332e7adce8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d69a60016b0794928667691b1b020b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f93cb988a297af2276ad",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "79b537d28ae94ca0e168",
+    "url": "/static/js/2.3c1975ae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3c1975ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f93cb988a297af2276ad",
+    "url": "/static/js/main.c73c6131.chunk.js"
+  },
+  {
+    "revision": "3e84209c9baa608f8b83",
+    "url": "/static/js/runtime-main.e6fcbbe7.js"
+  }
+]);
