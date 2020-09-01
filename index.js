4268d1c405c4b69ce5e461e6f72f026752dcded3
Robinson Elias Maqui Canaviri <rmaqui.trebolit@bancochile.cl>
2020-08-31 20:33:41 -0400

    Cors

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 require('dotenv').config();
-
+const cors = require('cors');
 const app = express();
 
 app.use('/api/auth', require('./routes/auth') );
+
+// CORS
+app.use(cors())
 
 //escuchar
 app.listen( process.env.PORT, () => {
