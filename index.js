f8c4374611d67e64000e3b42767f260283c6c3ed
Robinson Elias Maqui Canaviri <rmaqui.trebolit@bancochile.cl>
2020-08-31 21:51:55 -0400

    Public

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ app.use('/api/auth', require('./routes/auth') );
 // CORS
 app.use(cors());
 app.options('*', cors());
+
+//Directorio publico
+app.use( express.static('public'));
+
 //escuchar
 app.listen( process.env.PORT, () => {
     console.log(`Servidor corriendo en puerto ${ process.env.PORT }`);
